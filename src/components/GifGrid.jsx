323448df6565b60c86441126e4f00a4dd1666304
omarvc <168618697+omarviveros@users.noreply.github.com>
2024-08-30 16:03:14 -0600

    Rreact

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,39 @@
+
+import { Gifitem } from "./Gifitem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+
+
+export const GifGrid = ({category}) => {
+
+  const { images, isLoading } = useFetchGifs( category );
+
+  console.log({isLoading});
+
+
+ 
+
+
+  return (
+    <>
+    <h3>{category}</h3>
+    {
+      isLoading && (<h2>Cargando...</h2>)
+      
+    }
+  
+   
+    <div className="card-grid">
+      {
+        images.map( (image) => (
+          <Gifitem key={image.id}
+                   {...image}
+          />
+        ))
+      }
+        
+    </div>
+    
+    </>
+  )
+}
